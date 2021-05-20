81d05ef1e4f664d9bff782ebc8879ba83b80d14b
Arthur Wasenmueller <a.wasenmueller@gmail.com>
2021-05-20 16:46:21 +0200

    list controller halfway done

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,32 @@
 class ListsController < ApplicationController
+  # before_action :set_list, only: [:index, :show, :edit, :update]
+ 
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    list.save
+    redirect_to root_path
+  end
+
+#   private
+#     # Use callbacks to share common setup or constraints between actions.
+#     def set_list
+#       @list = List.find(params[:id])
+#     end
+
+    # Only allow a trusted parameter "white list" through.
+    def list_params
+      params.require(:list).permit(:name)
+    end
 end
